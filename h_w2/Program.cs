5b6c43a312cab1ab0a94da963c441505c3692bdb
Admin <eventarsier@gmail.com>
2022-11-24 22:57:48 +0300

    added some code x2

---
+++ h_w2/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите 1-ое число");
+ int a =  Convert.ToInt32(Console.ReadLine()); // 5
+
+Console.WriteLine("Введите 2-ое число");
+int b =  Convert.ToInt32(Console.ReadLine());  // 44
+
+Console.WriteLine("Введите 3-е число");
+int c =  Convert.ToInt32(Console.ReadLine());  // 78
+
+int maxNumber = a;
+
+if 
+(maxNumber < b)
+maxNumber = b; 
+
+if
+(maxNumber < c )
+maxNumber = c;
+
+Console.Write(maxNumber);
+   
+
+   
+
+
+
+
