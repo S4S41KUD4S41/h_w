9497ecbdd746702207891b77469a7635090504d7
Admin <eventarsier@gmail.com>
2022-11-24 23:25:23 +0300

    added some code x3 0))

---
+++ h_w3/Program.cs
@@ -0,0 +1,15 @@
+﻿    //Было трудно, но вроде работает, пришлось немного посмотреть какие знаки существуют (%, !=, и т.п) х)
+    Console.Clear();
+
+    Console.WriteLine("Введите число");
+    int a = Convert.ToInt32(Console.ReadLine());
+
+    if  
+    
+    (a % 2 == 0)
+    Console.WriteLine("Четное");
+
+    else if
+
+    (a != 0 );
+    Console.WriteLine("Нечетное");
