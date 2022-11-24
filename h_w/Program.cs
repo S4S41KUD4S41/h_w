bfd3c0294f2d005cfd5d3f0dd1709395840eee9c
Admin <eventarsier@gmail.com>
2022-11-24 22:28:54 +0300

    added some code

---
+++ h_w/Program.cs
@@ -0,0 +1,17 @@
+﻿// Возможно может показаться что я списал, но по правде я все делал сам только немножко посмотрел как такие задачи делать
+Console.Clear();
+
+Console.WriteLine("Введите 1-ое число");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите 2-ое число");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if 
+   (a > b && b < a)
+   Console.WriteLine(a);
+
+else
+    Console.WriteLine(b); 
+
+
